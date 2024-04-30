121504bf7a63f70b31c65cc75c99b1a434cdacf1
Spike Jourdain <spike.jourdain@acu.edu.au>
2024-04-30 13:56:24 +1000

    PromoPanel: responsiveness

--- src/app/components/PromoPanel/PromoPanel.tsx
+++ src/app/components/PromoPanel/PromoPanel.tsx
@@ -21,14 +21,14 @@ const PromoPanel = (props: TPromoPanelProps) => {
   return (
     <div
       className={cn(
-        'flex flex-1 items-center justify-between bg-acu-red-100 px-8 py-10 text-white [clip-path:polygon(0_0,calc(100%-2rem)_0,100%_2rem,100%_100%,0_100%)]',
+        'flex flex-1 flex-col items-center justify-between bg-acu-red-100 px-8 py-10 text-white sm:flex-row sm:[clip-path:polygon(0_0,calc(100%-2rem)_0,100%_2rem,100%_100%,0_100%)]',
         {
           [bgColor]: true,
           'w-fit flex-auto': width === 'content',
         },
       )}
     >
-      <span className="mr-4 text-2xl font-bold">{text}</span>
+      <span className="mb-6 text-2xl font-bold sm:mb-0 sm:mr-4">{text}</span>
       <ButtonCTA url={cta_url} text={cta_text} type="button" button_color={bg_color} />
     </div>
   );
