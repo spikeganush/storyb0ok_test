3be8f06a6734a1c184d6e6813c11d127a9fb1730
Spike Jourdain <spike.jourdain@acu.edu.au>
2024-08-06 11:39:42 +1000

    Refactor: #Removed millionLint in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,9 @@
-import MillionLint from '@million/lint';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   //Image optimization
   images: {
-    domains: ['vision2033.acu.edu.au']
-  }
+    domains: ['vision2033.acu.edu.au'],
+  },
 };
-export default MillionLint.next({
-  rsc: true
-})(nextConfig);
+
+export default nextConfig;
