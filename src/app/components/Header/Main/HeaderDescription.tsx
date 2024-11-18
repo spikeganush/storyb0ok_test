c919f48a4eeb74a7f6ac91a1032d231eab229a76
Spike <florian@playtoday.cc>
2024-11-18 11:46:10 +1100

    Add main header's description

---
+++ src/app/components/Header/Main/HeaderDescription.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const HeaderDescription = () => {
+  return <div>⚠️This component is a WIP. the responsiveness is not done.⚠️</div>;
+};
+
+export default HeaderDescription;
